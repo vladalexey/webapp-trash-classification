25d8e17af1f475aee65bdf64b534dcf369e97e16
Phan Quân <qmp23@drexel.edu>
2019-09-15 23:50:11 +0700

    Clean up and add comments

--- predict.py
+++ predict.py
@@ -2,27 +2,27 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
-import torch.optim as optim
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
-import time
-import os
-import copy
 from PIL import Image
 
-def createModel(num_classes=6):
+def createModel(num_classes=6, w_drop=True):
 
     model_ft = models.resnext101_32x8d(pretrained=True)
     num_ftrs = model_ft.fc.in_features
-    model_ft.fc = nn.Linear(num_ftrs, num_classes)
 
-    # model_ft.fc = nn.Sequential(
-    #     nn.Dropout(0.5),
-    #     nn.Linear(num_ftrs, num_classes)
-    # )
+    if not w_drop:
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+
+    else:
+        model_ft.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(num_ftrs, num_classes)
+        )
 
     return model_ft
+
 def get_transform():
 
     data_transform = transforms.Compose([
@@ -36,17 +36,21 @@ def get_transform():
     
 def setup(model_dir, model_class):
 
+    # Check if saved model is loaded with CPU or GPU machine
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     checkpoint = torch.load(model_dir, map_location=device)
 
+    # Build model structure and optimizer 
     predictor = model_class()
     opt = optim.SGD(predictor.parameters(), lr=0.001, momentum=0.9)
 
+    # Load model weights and optimizer states
     predictor.load_state_dict(checkpoint['state_dict'])
     opt.load_state_dict(checkpoint['optimizer'])
     epoch = 0
 
+    # Switch to evaluation mode
     predictor.eval()
 
     return predictor, opt, epoch
@@ -55,24 +59,24 @@ def predict(model, img, transform, epoch, classes=['cardboard', 'glass', 'metal'
     '''
         Do not change order of classes
     '''
+    # Open with pillow
     img = Image.open(img)
 
+    # Apply the same transform as training
     img_t = transform(img)
     batch_t = torch.unsqueeze(img_t, 0)
 
+    # Forward the img thru the model
     res = model(batch_t)
     _, idx = torch.max(res, 1)
 
     preds = torch.nn.functional.softmax(res, dim=1)[0] * 100
-
-    # print(classes[idx[0]], preds[idx[0]].item())
 
     return classes[idx[0]], preds[idx[0]].item(), preds
 
 if __name__ == "__main__":
 
     sample_inp = 'sample.jpg'
-
     model_dir = './models/models_resnext101_32x8d_acc_ 0.951807 loss_ 0.18151'
 
     data_transform = get_transform()
